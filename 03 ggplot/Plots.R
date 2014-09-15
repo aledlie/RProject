eee6e751d12720a2f7df6be1207bc3069219852b
alyshialedlie <alyshialedlie@gmail.com>
2014-09-15 13:27:56 -0500

    small edits to Plots.R

--- 03 ggplot/Plots.R
+++ 03 ggplot/Plots.R
@@ -21,9 +21,8 @@ InpatientVisits = rbind(InpatientVisits, dbGetQuery(con, "select * from mc_Inpat
 InpatientVisits = rbind(InpatientVisits, dbGetQuery(con, "select * from mc_InpatientVisits WHERE ID BETWEEN 90001 and 100000"))
 InpatientVisits = rbind(InpatientVisits, dbGetQuery(con, "select * from mc_InpatientVisits WHERE ID BETWEEN 100001 and 130000"))
 InpatientVisits = rbind(InpatientVisits, dbGetQuery(con, "select * from mc_InpatientVisits WHERE ID BETWEEN 130001 and 160000"))
-InpatientVisits = rbind(InpatientVisits, dbGetQuery(con, "select * from mc_InpatientVisits WHERE ID BETWEEN 160001 and 190000"))
 
-head(InPatientVisits)
+head(InpatientVisits)
 
 outpatientCostByCity = dbGetQuery(con, 
 "SELECT mc_Providers.City as City, AVG(mc_OutPatientVisits.AverageSubmittedCharges) as AvgBilledCost 
@@ -47,44 +46,44 @@ ON mc_Providers.ID = mc_OutPatientVisits.ProviderID
 GROUP BY mc_Providers.Name")
 
 outpatientCostByCity = dbGetQuery(con, 
-                                  "SELECT mc_Providers.City as City, AVG(mc_OutPatientVisits.AverageSubmittedCharges) as AvgBilledCost 
+"SELECT mc_Providers.City as City, AVG(mc_OutPatientVisits.AverageSubmittedCharges) as AvgBilledCost 
 FROM mc_OutPatientVisits 
 INNER JOIN mc_Providers 
 ON mc_Providers.ID = mc_OutPatientVisits.ProviderID 
 GROUP BY mc_Providers.City")
 
 outpatientCostByState = dbGetQuery(con, 
-                                   "SELECT mc_Providers.State as State, AVG(mc_OutPatientVisits.AverageSubmittedCharges) as AvgBilledCost 
+"SELECT mc_Providers.State as State, AVG(mc_OutPatientVisits.AverageSubmittedCharges) as AvgBilledCost 
 FROM mc_OutPatientVisits 
 INNER JOIN mc_Providers 
 ON mc_Providers.ID = mc_OutPatientVisits.ProviderID 
 GROUP BY mc_Providers.State")
 
-outpatientCostByHospital = dbGetQuery(con, "
-SELECT mc_Providers.Name as Hospital, AVG(mc_OutPatientVisits.AverageSubmittedCharges) as AvgBilledCost 
+outpatientCostByHospital = dbGetQuery(con, 
+"SELECT mc_Providers.Name as Hospital, AVG(mc_OutPatientVisits.AverageSubmittedCharges) as AvgBilledCost 
 FROM mc_OutPatientVisits 
 INNER JOIN mc_Providers 
 ON mc_Providers.ID = mc_OutPatientVisits.ProviderID 
 GROUP BY mc_Providers.Name")
 
-graph1 <- ggplot(texas, aes(y = Rating, x = AverageCharge)) + geom_point()
-graph2 <- ggplot(national, aes(y = Rating, x = AverageCharge)) + geom_point()
-graph3 <- ggplot(austin, aes(y = AnswerPercent, x = AverageCharge)) + geom_point()
-graph4 <- ggplot(nationalBYProcedure, aes(y = AverageCharge, x = Procedure)) + geom_boxplot()
-graph5 <- ggplot(AustinBYProcedure, aes(y = AverageCharge, x = Procedure)) + geom_boxplot()
-graph6 <- ggplot(HoustonByProcedure, aes(y = AverageCharge, x = Procedure)) + geom_boxplot()
-graph7 <- graph4 <- ggplot(HoustonByProcedure, aes(y = AverageCharge, x = Procedure)) + geom_violin() + coord_flip()
-graph2 + facet_wrap(~Procedure)
+InpatientCostByCity = dbGetQuery(con, 
+"SELECT mc_Providers.City as City, AVG(mc_InPatientVisits.CoveredCharges) as AvgBilledCost 
+FROM mc_InPatientVisits 
+INNER JOIN mc_Providers 
+ON mc_Providers.ID = mc_InPatientVisits.ProviderID 
+GROUP BY mc_Providers.City")
 
+InpatientCostByState = dbGetQuery(con, 
+"SELECT mc_Providers.State as State, AVG(mc_InpatientVisits.CoveredCharges) as AvgBilledCost 
+FROM mc_InpatientVisits 
+INNER JOIN mc_Providers 
+ON mc_Providers.ID = mc_InpatientVisits.ProviderID 
+GROUP BY mc_Providers.State")
 
-ggplot(data = diamonds) + geom_histogram(aes(x = carat))
-ggplot(data = diamonds) + geom_density(aes(x = carat, fill = "gray50"))
-ggplot(diamonds, aes(x = carat, y = price)) + geom_point()
-p <- ggplot(diamonds, aes(x = carat, y = price)) + geom_point(aes(color = color))
-p + facet_wrap(~color) # For ~, see http://stat.ethz.ch/R-manual/R-patched/library/base/html/tilde.html and http://stat.ethz.ch/R-manual/R-patched/library/stats/html/formula.html
-p + facet_grid(cut ~ clarity)
-p <- ggplot(diamonds, aes(x = carat)) + geom_histogram(aes(color = color), binwidth = max(diamonds$carat)/30)
-p + facet_wrap(~color) 
-p + facet_grid(cut ~ clarity)
-
+InpatientCostByHospital = dbGetQuery(con, "
+SELECT mc_Providers.Name as Hospital, AVG(mc_InPatientVisits.CoveredCharges) as AvgBilledCost 
+FROM mc_InPatientVisits 
+INNER JOIN mc_Providers 
+ON mc_Providers.ID = mc_InPatientVisits.ProviderID 
+GROUP BY mc_Providers.Name")
 
--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-RWorkshop
-=========
-=======
 RProject
 ========
 
 RProject for Medicare Data
->>>>>>> 3ea6205c57b62c218fcc1480707dcd2ba9a8773b

756e393c03833ce2cd29f817a0a9180593872ffb
Eric Tang <eric.tang.91@gmail.com>
2014-09-16 00:56:03 -0500

    cleaned up maps.r

--- 03 ggplot/maps.R
+++ 03 ggplot/maps.R
@@ -2,8 +2,8 @@ library("ggplot2")
 options(java.parameters="-Xmx2g")
 library(rJava)
 library(RJDBC)
-library(maps)
-library(mapproj)
+library(maps)   #need to install
+library(mapproj)#need to install
 library(zipcode)
 
 jdbcDriver <- JDBC(driverClass="oracle.jdbc.OracleDriver", classPath="C:/Program Files/Java/jdk1.7.0/ojdbc7.jar")
@@ -15,33 +15,13 @@ possibleError <- tryCatch(
 )
 if(!inherits(possibleError, "error")){
   #med <- dbGetQuery(jdbcConnection, "select * from TEMP where rownum <= 1000")
-  med <- dbGetQuery(jdbcConnection, "SELECT *
-FROM TEMP INNER JOIN mc_outpatientvisits
-ON temp.id = mc_outpatientvisits.providerid where mc_outpatientvisits.AVERAGESUBMITTEDCHARGES > 100")
+  med <- dbGetQuery(jdbcConnection, "SELECT * FROM TEMP INNER JOIN mc_outpatientvisits ON temp.id = mc_outpatientvisits.providerid where mc_outpatientvisits.AVERAGESUBMITTEDCHARGES > 100")
   dbDisconnect(jdbcConnection)
 }
 head(med)
 
-#data(med)
-#data(zipcode)
-#zipcode$region = substr(zipcode$zip, 1, 1)
-
-#g = ggplot(data=med) + geom_point(aes(x=LONGITUDE, y=LATITUDE, colour=STATE))
-
-# simplify display and limit to the "lower 48"
-#g = g + theme_bw() + scale_x_continuous(limits = c(-125,-66), breaks = NULL)
-#g = g + scale_y_continuous(limits = c(25,50), breaks = NULL)
-
-# don't need axis labels
-#g = g + labs(x=NULL, y=NULL)
-#g
-
-#gplot(med) + geom_histogram(aes(x = PROVIDERID))
-
-
 map(database= "usa", ylim=c(45,90), xlim=c(-160,-50), col="grey80", fill=TRUE, projection="gilbert", orientation= c(90,0,225))
-lon <- med$LONGITUDE  #fake longitude vector
-lat <- med$LATITUDE  #fake latitude vector
+lon <- med$LONGITUDE 
+lat <- med$LATITUDE  
 coord <- mapproject(lon, lat, proj="gilbert", orientation=c(90, 0, 225))  #convert points to projected lat/long
 points(coord, pch=20, cex=1.2, col=2)  #plot converted points
-

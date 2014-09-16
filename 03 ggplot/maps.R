570db2945e610f863a01c68185edc791e129d498
Eric Tang <eric.tang.91@gmail.com>
2014-09-16 01:30:05 -0500

    added query

--- 03 ggplot/maps.R
+++ 03 ggplot/maps.R
@@ -16,12 +16,24 @@ possibleError <- tryCatch(
 if(!inherits(possibleError, "error")){
   #med <- dbGetQuery(jdbcConnection, "select * from TEMP where rownum <= 1000")
   med <- dbGetQuery(jdbcConnection, "SELECT * FROM TEMP INNER JOIN mc_outpatientvisits ON temp.id = mc_outpatientvisits.providerid where mc_outpatientvisits.AVERAGESUBMITTEDCHARGES > 100")
+  med2 <- dbGetQuery(jdbcConnection, "SELECT * FROM TEMP INNER JOIN mc_outpatientvisits ON temp.id = mc_outpatientvisits.providerid where mc_outpatientvisits.AVERAGESUBMITTEDCHARGES < 100")
+  med3 <- dbGetQuery(jdbcConnection, "SELECT * FROM TEMP INNER JOIN mc_outpatientvisits ON temp.id = mc_outpatientvisits.providerid INNER JOIN mc_outpatientservices ON mc_outpatientservices.ID = mc_outpatientVisits.APCID where mc_outpatientservices.description = ?", "Level I Debridement & Destruction")
   dbDisconnect(jdbcConnection)
 }
-head(med)
+head(med3)
 
 map(database= "usa", ylim=c(45,90), xlim=c(-160,-50), col="grey80", fill=TRUE, projection="gilbert", orientation= c(90,0,225))
 lon <- med$LONGITUDE 
 lat <- med$LATITUDE  
 coord <- mapproject(lon, lat, proj="gilbert", orientation=c(90, 0, 225))  #convert points to projected lat/long
-points(coord, pch=20, cex=1.2, col=2)  #plot converted points
+points(coord, pch=20, cex=0.2, col=2)  #plot converted points
+
+lon <- med2$LONGITUDE 
+lat <- med2$LATITUDE  
+coord <- mapproject(lon, lat, proj="gilbert", orientation=c(90, 0, 225))  #convert points to projected lat/long
+points(coord, pch=20, cex=0.2, col=3)  #plot converted points
+
+lon <- med3$LONGITUDE 
+lat <- med3$LATITUDE  
+coord <- mapproject(lon, lat, proj="gilbert", orientation=c(90, 0, 225))  #convert points to projected lat/long
+points(coord, pch=20, cex=0.2, col=4)  #plot converted points
